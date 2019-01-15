5a10a5a37bef21c3663b725b14c063c33a5db7fd
JamesYech <jyech@jdtech.net>
2019-01-15 08:51:02 -0500

    Refactored bookCase into SFC

--- src/components/bookCase.js
+++ src/components/bookCase.js
@@ -1,35 +1,35 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import  ListBooks from './bookshelf.js'
 
-class BookCase extends Component {
-	static propTypes = {
-	    books: PropTypes.array.isRequired,
-	    onChangeShelf: PropTypes.func.isRequired
-  	}
 
-	render() {
-		const {books, onChangeShelf} = this.props
-	  	const shelves = [
-	  		{key:'currentlyReading', name:'Reading'},
-			{key:'wantToRead', name:'Want to Read'},
-			{key:'read', name:'Already Read'},
-			{key:'none', name:'Recently Removed'}
-	  	]
+const BookCase = props => {
 
-		return (
-			<div>
-				{shelves.map((shelf, i) =>
-				  	<ListBooks
-			  			key={i}
-		  				books={books.filter((b) => b.shelf===shelf.key)}
-		  				shelf={shelf.name}
-		  				onChangeShelf={onChangeShelf}
-		  			/>
-				)}
-			</div>
-		 )
-	}
+	const {books, onChangeShelf} = props
+  	const shelves = [
+  		{key:'currentlyReading', name:'Reading'},
+		{key:'wantToRead', name:'Want to Read'},
+		{key:'read', name:'Already Read'},
+		{key:'none', name:'Recently Removed'}
+  	]
+
+	return (
+		<div>
+			{shelves.map((shelf, i) =>
+			  	<ListBooks
+		  			key={i}
+	  				books={books.filter((b) => b.shelf===shelf.key)}
+	  				shelf={shelf.name}
+	  				onChangeShelf={onChangeShelf}
+	  			/>
+			)}
+		</div>
+	)
+}
+
+BookCase.propTypes = {
+	books: PropTypes.array.isRequired,
+	onChangeShelf: PropTypes.func.isRequired
 }
 
 export default BookCase
